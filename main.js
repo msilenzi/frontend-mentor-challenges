601fa2fff1c4ddfe1ddaa713d629f84159a4b257
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-07-20 17:11:26 -0300

    add challenges list

---
+++ main.js
@@ -0,0 +1,45 @@
+const challenges = [
+  {
+    name: "Recipe Page",
+    imageSrc: "./01-recipe-page/screenshot-recipe-page-msilenzi.png",
+    href: "01-recipe-page",
+    languages: ["html", "css"],
+  },
+];
+
+const fragment = document.createDocumentFragment();
+
+challenges.forEach(({ name, imageSrc, href, languages }) => {
+  const li = document.createElement("li");
+  li.className = "card";
+  li.style.backgroundImage = `url('${imageSrc}')`;
+  fragment.appendChild(li);
+
+  const a = document.createElement("a");
+  a.href = href;
+  li.appendChild(a);
+
+  const cardBody = document.createElement("div");
+  cardBody.className = "card__body";
+  a.appendChild(cardBody);
+
+  const title = document.createElement("h2");
+  title.className = "card__title";
+  title.textContent = name;
+  cardBody.appendChild(title);
+
+  const languagesList = document.createElement("ul");
+  languagesList.className = "card__languages";
+  cardBody.appendChild(languagesList);
+
+  languages.forEach((language) => {
+    const languageItem = document.createElement("li");
+    languageItem.className = "card__language";
+    languageItem.dataset.language = language;
+    languageItem.textContent = language;
+    languagesList.appendChild(languageItem);
+  });
+});
+
+const cardList = document.getElementById("card-list");
+cardList.appendChild(fragment);
