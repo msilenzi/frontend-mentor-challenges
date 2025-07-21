edb4be2a218f3550bb0e2a8969cace5c9b773c4f
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-07-21 13:12:07 -0300

    improve 04-contact-form form validation

--- 04-contact-form/main.js
+++ 04-contact-form/main.js
@@ -1,58 +1,151 @@
+/**
+ * @typedef {HTMLInputElement | HTMLTextAreaElement | NodeListOf} Field
+ */
+
+/**
+ * @typedef {Object} Validator
+ * @property {(field: Field) => boolean} validator
+ * @property {string} message
+ */
+
+/**
+ * @typedef {Object} FieldValidator
+ * @property {Field} field
+ * @property {Validator[]} validators
+ */
+
 /** @type {HTMLFormElement} */
 const $form = document.getElementById("contact-form");
 
-/** @type {HTMLInputElement} */
-const $firstName = document.getElementById("first-name");
-
-/** @type {HTMLInputElement} */
-const $lastName = document.getElementById("last-name");
-
-/** @type {HTMLInputElement} */
-const $email = document.getElementById("email");
-
-/** @type {NodeListOf<HTMLInputElement>} */
-const $queryTypes = document.getElementsByName("query-type");
-
-/** @type {HTMLTextAreaElement} */
-const $message = document.getElementById("message");
-
-/** @type {HTMLInputElement} */
-const $contactConsent = document.getElementById("contact-consent");
+/** @type {FieldValidator[]} */
+const validations = [
+  {
+    field: document.getElementById("first-name"),
+    validators: [
+      {
+        validator: (field) => isEmptyString(field.value),
+        message: "This field is required",
+      },
+    ],
+  },
+  {
+    field: document.getElementById("last-name"),
+    validators: [
+      {
+        validator: (field) => isEmptyString(field.value),
+        message: "This field is required",
+      },
+    ],
+  },
+  {
+    field: document.getElementById("email"),
+    validators: [
+      {
+        validator: (field) => isEmptyString(field.value),
+        message: "This field is required",
+      },
+      {
+        validator: (field) => !isValidEmail(field.value),
+        message: "Please enter a valid email address",
+      },
+    ],
+  },
+  {
+    field: document.getElementsByName("query-type"),
+    validators: [
+      {
+        validator: (fields) => [...fields].every((radio) => !radio.checked),
+        message: "Please select a query type",
+      },
+    ],
+  },
+  {
+    field: document.getElementById("message"),
+    validators: [
+      {
+        validator: (field) => isEmptyString(field.value),
+        message: "This field is required",
+      },
+    ],
+  },
+  {
+    field: document.getElementById("contact-consent"),
+    validators: [
+      {
+        validator: (field) => !field.checked,
+        message: "To submit this form, please consent to being contacted",
+      },
+    ],
+  },
+];
 
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   clearAllErrors($form);
 
-  if (isEmptyString($firstName.value)) {
-    showError($firstName, "This field is required");
-  }
-
-  if (isEmptyString($lastName.value)) {
-    showError($lastName, "This field is required");
-  }
-
-  if (isEmptyString($email.value)) {
-    showError($email, "This field is required");
-  } else if (!isValidEmail($email.value)) {
-    showError($email, "Please enter a valid email address");
-  }
-
-  if ([...$queryTypes].every((type) => !type.checked)) {
-    showError($queryTypes[0], "Please select a query type");
-  }
-
-  if (isEmptyString($message.value)) {
-    showError($message, "This field is required");
-  }
-
-  if (!$contactConsent.checked) {
-    showError(
-      $contactConsent,
-      "To submit this form, please consent to being contacted"
-    );
+  if (isFormValid(validations)) {
+    console.log("formulario válido");
+  } else {
+    console.log("formulario inválido");
   }
 });
+
+//#region form handler =========================================================
+
+/**
+ * @param {FieldValidator[]} validations
+ * @returns {boolean}
+ */
+function isFormValid(validations) {
+  let formIsValid = true;
+  validations.forEach((fieldValidation) => {
+    if (!isFieldValid(fieldValidation)) formIsValid = false;
+  });
+  return formIsValid;
+}
+
+/**
+ * @param {FieldValidator} fieldValidator
+ * @returns {boolean}
+ */
+function isFieldValid({ field, validators }) {
+  for (const { validator, message } of validators) {
+    if (validator(field)) {
+      showError(field, message);
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * @param {Field} field
+ * @param {string} errorMessage
+ */
+function showError(field, errorMessage) {
+  const element = field instanceof NodeList ? field[0] : field;
+  const group = element.closest(".validation-required");
+  const error = group.querySelector(".form__error");
+  group.classList.add("has-errors");
+  error.textContent = errorMessage;
+}
+
+/**
+ * @param {HTMLFormElement} form
+ * @returns {void}
+ */
+function clearAllErrors(form) {
+  const groups = form.querySelectorAll(".has-errors");
+  for (const group of groups) {
+    group.classList.remove("has-errors");
+    const errorMessage = group.querySelector(".form__error");
+    errorMessage.textContent = "";
+  }
+}
+
+//#endregion
+
+//#region validators ===========================================================
 
 /**
  * @param {string} value
@@ -71,27 +164,4 @@ function isValidEmail(email) {
   return emailRegex.test(email);
 }
 
-/**
- *
- * @param {HTMLElement} input
- * @param {string} errorMessage
- */
-function showError(input, errorMessage) {
-  const group = input.closest(".validation-required");
-  group.classList.add("has-errors");
-
-  const error = group.querySelector(".form__error");
-  console.log(error);
-  error.textContent = errorMessage;
-}
-
-function clearAllErrors(form) {
-  const groups = form.querySelectorAll(".has-errors");
-
-  for (const group of groups) {
-    group.classList.remove("has-errors");
-
-    const errorMessage = group.querySelector(".form__error");
-    errorMessage.textContent = "";
-  }
-}
+//#endregion
